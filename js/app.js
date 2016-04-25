86efea8f501b5452861f71a513ed1bc2c1db65fc
Mauro Joel Schutz <maurojoel@outlook.com>
2016-04-24 22:12:48 -0300

    Adicionando suporte a API History

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 $(function () {
 
+	function supports_history_api() {
+	  return !!(window.history && history.pushState);
+	}
 
-    function loadContent(doc) {
+    function loadContent(doc) {	
         var url = "../html/" + doc;
 
         $.get(url, function (content) {
@@ -18,13 +21,29 @@ $(function () {
     function init() {
         loadContent("home.html");
 
-        $(".nav.navbar-nav").find("a").on("click", function () {
-            var data = $(this).data();
-            loadContent(data.load);
-            $(".active").removeClass("active");
-            $(this).parent().addClass("active");
-
+        $(".navbar a").on("click", function (evt) {
+			evt.preventDefault();
+			
+            var page = $(this).attr('href');
+			if (supports_history_api()) { history.pushState({state: 'new'}, '', page); }
+            loadContent(page);
+			if (page != 'home.html') {
+				$(".active").removeClass("active");
+				$(this).parent().addClass("active");
+			}
+			
+			return false;
         });
+		
+		if (supports_history_api())
+		window.onpopstate = function(evt) {
+			var page = window.location.pathname.substring(1);
+			$(".navbar a").parent().removeClass("active");
+			if (page != 'home.html') {
+				$('a[href="' + page + '"]').parent().addClass("active");
+			}
+			loadContent(page);
+		};
 
     }
 
